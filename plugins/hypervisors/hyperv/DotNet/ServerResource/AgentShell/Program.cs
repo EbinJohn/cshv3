0544482371e17ea4ed20c6b25fe37400f471f528
Don Lafferty <dlafferty@gmail.com>
2013-04-18 22:07:46 +0100

    Add source and project files for Hyper-V agent. As a general guid, *.cs files are source, *.csproj file is Visual Studio project that contains build settings and instructions, settings of all kinds are in XML, *.Designer.* files are generated by Visual Studio for its various editors.
    More details...
    packages.config references NuGet library packages that the project relies on.
    Settings for the agent are held in AgentShell.settings in XML format
    AgentShell.Designer.cs includes wrappers for easy access to the settings.
    Service1 subytypes ServiceBase, which provides it with all the hooks to be run as a Windows Service, which is the Windows equivalent of a Daemon process.  The name is unfortunate, and may change.  The service uses a trick whereby it can be run as a Console application for debugging in addition to being capable of being run as a Windows Service.  The reason is that you cannot run a Windows Service direcdly.  It has to be started by the Service Control Manager (SCM), which gets in the way of developers.  The decision to run as a console application or a Windows Service is taken in Program.cs on the basis of a command line arguement.

---
+++ plugins/hypervisors/hyperv/DotNet/ServerResource/AgentShell/Program.cs
@@ -0,0 +1,41 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudStack.Plugin.AgentShell
+{
+    static class Program
+    {
+        private static ILog logger = LogManager.GetLogger(typeof(Program));
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(params string[] args)
+        {
+            string arg1 = string.Empty;
+
+            if (args.Length > 0)
+            {
+                arg1 = args[0];
+                logger.DebugFormat("CloudStack ServerResource arg is ", arg1);
+            }
+
+            if (string.Compare(arg1, "--console", true) == 0)
+            {
+                logger.InfoFormat("CloudStack ServerResource running as console app");
+                new Service1().RunConsole(args);
+            }
+            else
+            {
+                logger.InfoFormat("CloudStack ServerResource running as Windows Service");
+                ServiceBase[] ServicesToRun = new ServiceBase[] { new Service1() };
+                ServiceBase.Run(ServicesToRun);
+            }
+        }
+    }
+}
---
+++ plugins/hypervisors/hyperv/DotNet/ServerResource/AgentShell/Service1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
+using System.Web.Http;
+using log4net;
+
+namespace CloudStack.Plugin.AgentShell
+{
+    public partial class Service1 : ServiceBase
+    {
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool AllocConsole();
+
+        [System.Runtime.InteropServices.DllImport("kernel32.dll", SetLastError = true)]
+        static extern bool FreeConsole();
+
+        HttpSelfHostServer server;
+
+        private static ILog logger = LogManager.GetLogger(typeof(Service1));
+
+        public Service1()
+        {
+            InitializeComponent();
+
+            this.ServiceName = "CloudStack ServerResource";
+
+            // TODO:  Update to use configuration
+            Uri baseUri = new Uri("http://localhost:8080");
+
+            var config = new HttpSelfHostConfiguration(baseUri);
+
+            // TODO:  Update to retrieve from ServerResource
+            config.Routes.MapHttpRoute(
+                "API Default", "api/{controller}/{id}",
+                new { id = RouteParameter.Optional });
+
+            // TODO: update to catch and log exceptions 
+            server = new HttpSelfHostServer(config);
+
+            // TODO: configure logging
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            server.OpenAsync().Wait();
+        }
+
+        protected override void OnStop()
+        {
+            server.CloseAsync().Wait();
+        }
+
+        internal void RunConsole(string[] args)
+        {
+            OnStart(args);
+
+            AllocConsole();
+
+            Console.WriteLine("Service running ... press <ENTER> to stop");
+
+            Console.ReadLine();
+
+            FreeConsole();
+
+            OnStop();
+        }
+    }
+}
