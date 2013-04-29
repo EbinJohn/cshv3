b50bbbb41248b1ddd133ca8f7e9e18227c283024
Don Lafferty <dlafferty@gmail.com>
2013-04-29 11:55:47 +0100

    Test framework for Hyperv ServerResource.  Uses VisualStudio Unit Test support.

---
+++ plugins/hypervisors/hyperv/DotNet/ServerResource/ServerResource.Tests/HypervResourceControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ServerResource;
+using ServerResource.Controllers;
+
+namespace ServerResource.Tests.Controllers
+{
+    [TestClass]
+    public class HypervResourceControllerTest
+    {
+        [TestMethod]
+        public void Post()
+        {
+            // Arrange
+            VirtualMachineController controller = new VirtualMachineController();
+
+            // Act
+            controller.Post("value");
+
+            // Assert
+        }
+    }
+}
