923cafbe9715b03468bef958b00f5dea92ca10b2
Don Lafferty <dlafferty@gmail.com>
2013-05-10 01:31:37 +0100

    Add ability to generate the StartupCommand[] required by the ServerComponent when a host is first added to CloudStack.

---
+++ plugins/hypervisors/hyperv/DotNet/ServerResource/HypervResource/CloudStackTypes.cs
@@ -0,0 +1,115 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// C# versions of certain CloudStack types to simplify JSON serialisation.
+// Limit to the number of types, becasue they are written and maintained manually.
+// JsonProperty used to identify property name when serialised, which allows
+// later adoption of C# naming conventions if requried. 
+namespace HypervResource
+{
+        public enum StoragePoolType {
+            /// <summary>
+            /// local directory
+            /// </summary>
+            Filesystem,         
+            /// <summary>
+            /// NFS or CIFS 
+            /// </summary>
+            NetworkFilesystem,  
+            /// <summary>
+            /// shared LUN, with a clusterfs overlay 
+            /// </summary>
+            IscsiLUN,           
+            /// <summary>
+            /// for e.g., ZFS Comstar 
+            /// </summary>
+            Iscsi,              
+            /// <summary>
+            /// for iso image
+            /// </summary>
+            ISO,                
+            /// <summary>
+            /// XenServer local LVM SR
+            /// </summary>
+            LVM, 
+            /// <summary>
+            /// 
+            /// </summary>
+            CLVM, 
+            /// <summary>
+            /// 
+            /// </summary>
+            RBD, 
+            /// <summary>
+            /// 
+            /// </summary>
+            SharedMountPoint, 
+            /// <summary>
+            /// VMware VMFS storage 
+            /// </summary>
+            VMFS, 
+            /// <summary>
+            /// for XenServer, Storage Pool is set up by customers. 
+            /// </summary>
+            PreSetup, 
+            /// <summary>
+            /// XenServer local EXT SR 
+            /// </summary>
+            EXT, 
+            /// <summary>
+            /// 
+            /// </summary>
+            OCFS2
+    }
+
+        public enum StorageResourceType
+        {
+            STORAGE_POOL, STORAGE_HOST, SECONDARY_STORAGE, LOCAL_SECONDARY_STORAGE
+        }
+
+
+    public struct StoragePoolInfo
+    {
+        [JsonProperty("uuid")]
+        public String uuid;
+        [JsonProperty("host")]
+        String host;
+        [JsonProperty("localPath")]
+        String localPath;
+        [JsonProperty("hostPath")]
+        String hostPath;
+        [JsonProperty("poolType")]
+        string poolType;
+        [JsonProperty("capacityBytes")]
+        long capacityBytes;
+        [JsonProperty("availableBytes")]
+        long availableBytes;
+        [JsonProperty("details")]
+        Dictionary<String, String> details;
+
+        public StoragePoolInfo(String uuid, String host, String hostPath,
+                String localPath, string poolType, long capacityBytes,
+                long availableBytes)
+        {
+            this.uuid = uuid;
+            this.host = host;
+            this.localPath = localPath;
+            this.hostPath = hostPath;
+            this.poolType = poolType;
+            this.capacityBytes = capacityBytes;
+            this.availableBytes = availableBytes;
+            details = null;
+        }
+
+        public StoragePoolInfo(String uuid, String host, String hostPath,
+                String localPath, string poolType, long capacityBytes,
+                long availableBytes, Dictionary<String, String> details) : this(uuid, host, hostPath, localPath, poolType, capacityBytes, availableBytes)
+        {
+            this.details = details;
+        }
+    }
+}
--- plugins/hypervisors/hyperv/DotNet/ServerResource/HypervResource/HypervResourceController.cs
+++ plugins/hypervisors/hyperv/DotNet/ServerResource/HypervResource/HypervResourceController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using log4net;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -7,8 +10,40 @@ using System.Web.Http;
 
 namespace HypervResource
 {
+
+    public struct HypervResourceControllerConfig
+    {
+        public string PrivateIpAddress;
+        public string GatewayIpAddress;
+        public string PrivateMacAddress;
+        public string PrivateNetmask;
+        public string StorageNetmask;
+        public string StorageMacAddress;
+        public string StorageIpAddress;
+        public long RootDeviceReservedSpace;
+        public string RootDeviceName;
+        public ulong ParentPartitionMinMemoryMb;
+    }
+
+    // Supports HTTP GET and HTTP POST
+    // POST takes dynamic to allow it to receive JSON without concern for what is the underlying object.
+    // E.g. http://stackoverflow.com/questions/14071715/passing-dynamic-json-object-to-web-api-newtonsoft-example 
+    // and http://stackoverflow.com/questions/3142495/deserialize-json-into-c-sharp-dynamic-object
+    // Use ActionName attribute to allow multiple POST URLs, one for each supported command
+    // E.g. http://stackoverflow.com/a/12703423/939250
+    // Strictly speaking, this goes against the purpose of an ApiController, which is to provide one GET/POST/PUT/DELETE, etc.
+    // However, it reduces the amount of code by removing the need for a switch according to the incoming command type.
     public class HypervResourceController : ApiController
     {
+        public static void Configure(HypervResourceControllerConfig config)
+        {
+            HypervResourceController.config = config;
+        }
+
+        static HypervResourceControllerConfig config = new HypervResourceControllerConfig();
+
+        private static ILog logger = LogManager.GetLogger(typeof(WmiCalls));
+
         public static void Initialize()
         {
         }
@@ -16,16 +51,124 @@ namespace HypervResource
         // GET api/HypervResource
         // Useful for testing!
         //
-        public IEnumerable<string> Get()
+        public string Get()
         {
-            return new string[] { "value1", "value2" };
+            return "HypervResource controller, use POST to send send JSON encoded objects";
         }
 
-        // POST api/HypervResource
-        public void Post([FromBody]string value)
+        // POST api/HypervResource/StartCommand
+        [ActionName("StartCommand")]
+        public JContainer StartCommand([FromBody]dynamic cmd)
         {
-            // TODO:  Dispatch according to command
+            // TODO: Error handling, and return details to allow CloudStack to create the answer
+            WmiCalls.DeployVirtualMachine(cmd);
+            return cmd;
+        }
 
+        // POST api/HypervResource/StartCommand
+        [ActionName("StopCommand")]
+        public JContainer StopCommand([FromBody]dynamic cmd)
+        {
+            // TODO: Error handling, and return details to allow CloudStack to create the answer
+            WmiCalls.DestroyVm(cmd);
+            return cmd;
+        }
+
+        // POST api/HypervResource/StartupCommand
+        [ActionName("StartupCommand")]
+        public JContainer StartupCommand([FromBody]dynamic cmdArray)
+        {
+            // Log agent configuration
+            logger.Info("Agent StartupRoutingCommand received " + cmdArray.ToString());
+            dynamic strtRouteCmd = cmdArray[0].StartupRoutingCommand;
+
+            // Insert networking details
+            strtRouteCmd.privateIpAddress = config.PrivateIpAddress;
+            strtRouteCmd.privateNetmask = config.PrivateNetmask;
+            strtRouteCmd.privateMacAddress = config.PrivateMacAddress;
+            strtRouteCmd.storageIpAddress = config.PrivateIpAddress;
+            strtRouteCmd.storageNetmask = config.PrivateNetmask;
+            strtRouteCmd.storageMacAddress = config.PrivateMacAddress;
+            strtRouteCmd.gatewayIpAddress = config.GatewayIpAddress;
+
+            // Detect CPUs, speed, memory
+            uint cores;
+            uint mhz;
+            WmiCalls.GetProcessorResources(out cores, out mhz);
+            strtRouteCmd.cpus = cores;
+            strtRouteCmd.speed = mhz;
+            ulong memory_mb;
+            WmiCalls.GetMemoryResources(out memory_mb);
+            strtRouteCmd.memory = memory_mb;
+
+            // Need 2 Gig for DOM0, see http://technet.microsoft.com/en-us/magazine/hh750394.aspx
+            strtRouteCmd.dom0MinMemory = config.ParentPartitionMinMemoryMb;
+            
+
+            // Insert storage pool details.
+            //
+            // Read the localStoragePath for virtual disks from the Hyper-V configuration
+            // See http://blogs.msdn.com/b/virtual_pc_guy/archive/2010/05/06/managing-the-default-virtual-machine-location-with-hyper-v.aspx
+            // for discussion of Hyper-V file locations paths.
+            string localStoragePath =WmiCalls.GetDefaultVirtualDiskFolder();
+            if (localStoragePath != null)
+            {
+                // GUID arbitrary.  Host agents deals with storage pool in terms of localStoragePath.
+                // We use HOST guid.
+                string poolGuid = strtRouteCmd.guid;
+
+                if (poolGuid == null)
+                {
+                    poolGuid = Guid.NewGuid().ToString();
+                    logger.InfoFormat("Setting Startup StoragePool GUID to " + poolGuid);
+                }
+                else
+                {
+                    logger.InfoFormat("Setting Startup StoragePool GUID same as HOST, i.e. " + poolGuid);
+                }
+
+                // NB: DriveInfo does not work for remote folders (http://stackoverflow.com/q/1799984/939250)
+                System.IO.DriveInfo poolInfo = new System.IO.DriveInfo(localStoragePath);
+                long capacity = poolInfo.TotalSize;
+                long available = poolInfo.AvailableFreeSpace;
+
+                // Don't allow all of the Root Device to be used for virtual disks
+                if (poolInfo.RootDirectory.Name.ToLower().Equals(config.RootDeviceName))
+                {
+                    available -= config.RootDeviceReservedSpace;
+                    available = available > 0 ? available : 0;
+                    capacity -= config.RootDeviceReservedSpace;
+                    capacity = capacity > 0 ? capacity : 0;
+                }
+                string ipAddr = strtRouteCmd.privateIpAddress;
+                StoragePoolInfo pi = new StoragePoolInfo(
+                    poolGuid.ToString(),
+                    ipAddr,
+                    localStoragePath,
+                    localStoragePath,
+                    StoragePoolType.Filesystem.ToString(),
+                    capacity,
+                    available);
+
+                // Build StorageStartCommand using an anonymous type
+                // See http://stackoverflow.com/a/6029228/939250
+                var startupStorageCommand = new
+                    {
+                        StartupStorageCommand = new
+                            {
+                                poolInfo = pi,
+                                guid = pi.uuid,
+                                dataCenter = strtRouteCmd.dataCenter,
+                                resourceType = StorageResourceType.STORAGE_POOL.ToString()
+                            }
+                    };
+                JToken tok = JToken.FromObject(startupStorageCommand);
+                cmdArray.Add(tok);
+            }
+
+            // Convert result to array for type correctness?
+            logger.Info("Sending updated StartupRoutingCommand " + cmdArray.ToString());
+            return cmdArray;
         }
     }
 }
