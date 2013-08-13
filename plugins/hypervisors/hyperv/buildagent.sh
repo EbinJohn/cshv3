70c75e7dcdbfd20d2aedbf4c935297df30e6970f
lafferty <dlafferty@gmail.com>
2013-08-13 17:50:45 +0100

    Update to always restore the NuGet packages.

--- plugins/hypervisors/hyperv/buildagent.sh
+++ plugins/hypervisors/hyperv/buildagent.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export EnableNuGetPackageRestore=true
 wget http://nuget.org/nuget.exe
 mv nuget.exe ./DotNet/ServerResource/.nuget/NuGet.exe
 chmod a+x ./DotNet/ServerResource/.nuget/NuGet.exe
